d1e97bff6ff43bd26030119ab85c377ce2db107c
naguar <roland.jigert@gmail.com>
2018-06-29 20:45:42 +0200

    Restructured and added functions for more content.
    New code:
    - Cast Photos
    - Director
    - Genres
    
    Updated:
    - Cast is filtered to remove links/content not related to cast names.
    - Product info div to match different div locations, first code missed many releases.
    - Code formatting

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,5 @@
-# AdultDVDEmpire
-# Update: 12 July 2015
+# AdultDVDEmpire - nag
+# Update: 29 July 2018
 # Description: Updated for the changes to the new site.
 
 # URLS
@@ -7,8 +7,11 @@ ADE_BASEURL = 'http://www.adultdvdempire.com'
 ADE_SEARCH_MOVIES = ADE_BASEURL + '/dvd/search?q=%s'
 ADE_MOVIE_INFO = ADE_BASEURL + '/%s/'
 
+INITIAL_SCORE = 100
+GOOD_SCORE = 98
+
 def Start():
-  HTTP.CacheTime = CACHE_1DAY
+  HTTP.CacheTime = CACHE_1MINUTE
   HTTP.SetHeader('User-agent', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)')
 
 class ADEAgent(Agent.Movies):
@@ -17,12 +20,11 @@ class ADEAgent(Agent.Movies):
   primary_provider = True
   accepts_from = ['com.plexapp.agents.localmedia']
 
-
   def search(self, results, media, lang):
     title = media.name
     if media.primary_metadata is not None:
       title = media.primary_metadata.title
-    
+
     query = String.URLEncode(String.StripDiacritics(title.replace('-','')))
     # Finds div with class=item
     for movie in HTML.ElementFromURL(ADE_SEARCH_MOVIES % query).xpath('//div[contains(@class, "col-xs-7")]/h3/a[1]'): 
@@ -33,10 +35,10 @@ class ADEAgent(Agent.Movies):
 
       # curID = the ID portion of the href in 'movie'
       curID = movie.get('href').split('/',2)[1]
-      score = 100 - Util.LevenshteinDistance(title.lower(), curName.lower())
+      score = INITIAL_SCORE - Util.LevenshteinDistance(title.lower(), curName.lower())
       if curName.lower().count(title.lower()):
-        results.Append(MetadataSearchResult(id = curID, name = curName, score = 100, lang = lang))
-      elif (score >= 95):
+        results.Append(MetadataSearchResult(id = curID, name = curName, score = INITIAL_SCORE, lang = lang))
+      elif (score >= GOOD_SCORE):
         results.Append(MetadataSearchResult(id = curID, name = curName, score = score, lang = lang))
 
     results.Sort('score', descending=True)
@@ -53,8 +55,7 @@ class ADEAgent(Agent.Movies):
       thumb = HTTP.Request(thumbUrl)
       posterUrl = img.get('src')
       metadata.posters[posterUrl] = Proxy.Preview(thumb)
-    except:
-      pass
+    except: pass
 
     # Tagline
     try: metadata.tagline = html.xpath('//p[@class="Tagline"]')[0].text_content().strip()
@@ -62,7 +63,7 @@ class ADEAgent(Agent.Movies):
 
     # Summary.
     try:
-      for summary in html.xpath('//*[@id="content"]/div[2]/div[2]/div/p'):
+      for summary in html.xpath('//*[@class="product-details-container"]/div/div/p'):
         metadata.summary = summary.text_content()
     except Exception, e:
       Log('Got an exception while parsing summary %s' %str(e))
@@ -70,7 +71,17 @@ class ADEAgent(Agent.Movies):
     # Product info div
     data = {}
 
-    productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[2]/div[4]/div/div[1]/ul')[0])
+    # Match diffrent code, some titles are missing parts -- Still fails and needs to be refined.
+    if html.xpath('//*[@id="content"]/div[2]/div[3]/div/div[1]/ul'):
+      productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[2]/div[3]/div/div[1]/ul')[0])    
+    if html.xpath('//*[@id="content"]/div[2]/div[4]/div/div[1]/ul'):
+      productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[2]/div[4]/div/div[1]/ul')[0])
+    if html.xpath('//*[@id="content"]/div[2]/div[2]/div/div[1]/ul'):
+      productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[2]/div[2]/div/div[1]/ul')[0])
+    if html.xpath('//*[@id="content"]/div[3]/div[3]/div/div[1]/ul'):
+      productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[3]/div[3]/div/div[1]/ul')[0])
+    if html.xpath('//*[@id="content"]/div[3]/div[4]/div/div[1]/ul'):
+      productinfo = HTML.StringFromElement(html.xpath('//*[@id="content"]/div[3]/div[4]/div/div[1]/ul')[0])
 
     productinfo = productinfo.replace('<small>', '|')
     productinfo = productinfo.replace('</small>', '')
@@ -96,15 +107,62 @@ class ADEAgent(Agent.Movies):
         metadata.originally_available_at = Datetime.ParseDate(data['Released']).date()
         metadata.year = metadata.originally_available_at.year
       except: pass
- 
+
     # Cast
     try:
       metadata.roles.clear()
-      htmlcast = html.xpath('//a[contains(@class, "PerformerName")]')
-      for cast in htmlcast:
-        cname = cast.text_content().strip()
-        if (len(cname) > 0):
-          role = metadata.roles.new()
-          role.name = cname
+      if html.xpath('//*[contains(@class, "cast listgrid item-cast-list")]'):
+        htmlcast = HTML.StringFromElement(html.xpath('//*[contains(@class, "cast listgrid item-cast-list")]')[0])
+		
+		# -- Terrible setup but works for now.
+        htmlcast = htmlcast.replace('\n', '|').replace('\r', '').replace('\t', '').replace(');">', 'DIVIDER')
+        htmlcast = htmlcast.replace('<span>', '').replace('</span>', '')
+        htmlcast = htmlcast.replace('<li>', '').replace('</li>', '')
+        htmlcast = htmlcast.replace('<small>Director</small>', '')
+		
+		# Change to high res img -- This part need to be made better.
+        htmlcast = htmlcast.replace('t.jpg', 'h.jpg')
+        htmlcast = htmlcast.replace('<img src="https://imgs2cdn.adultempire.com/res/pm/pixel.gif" alt="" title="" class="img-responsive headshot" style="background-image:url(', '|')
+        htmlcast = HTML.ElementFromString(htmlcast).text_content()
+        htmlcast = htmlcast.split('|')
+        htmlcast = htmlcast[1:]
+        for cast in htmlcast:
+          if (len(cast) > 0):
+            imgURL, nameValue = cast.split('DIVIDER')
+            role = metadata.roles.new()
+            role.name = nameValue
+            role.photo = imgURL
     except Exception, e:
       Log('Got an exception while parsing cast %s' %str(e))
+     
+    # Director
+    try:
+      metadata.directors.clear()
+      if html.xpath('//a[contains(@label, "Director - details")]'):    
+        htmldirector = HTML.StringFromElement(html.xpath('//a[contains(@label, "Director - details")]')[0])
+        htmldirector = HTML.ElementFromString(htmldirector).text_content().strip()
+        if (len(htmldirector) > 0):
+          director = metadata.directors.new()
+          director.name = htmldirector
+    except Exception, e:
+      Log('Got an exception while parsing director %s' %str(e))
+
+    # Genres
+    try:
+      metadata.genres.clear()
+      if html.xpath('//*[contains(@class, "col-sm-4 spacing-bottom")]'):
+        htmlgenres = HTML.StringFromElement(html.xpath('//*[contains(@class, "col-sm-4 spacing-bottom")]')[2])
+        htmlgenres = htmlgenres.replace('\n', '|')
+        htmlgenres = htmlgenres.replace('\r', '')
+        htmlgenres = htmlgenres.replace('\t', '')
+        htmlgenres = HTML.ElementFromString(htmlgenres).text_content()
+        htmlgenres = htmlgenres.split('|')
+        htmlgenres = filter(None, htmlgenres)
+        htmlgenres = htmlgenres[1:]
+        htmlgenres = htmlgenres[:-1]
+        for gname in htmlgenres:
+          if len(gname) > 0:
+            metadata.genres.add(gname)
+    except Exception, e:
+      Log('Got an exception while parsing genres %s' %str(e))
+
